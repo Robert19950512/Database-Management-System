2f832fc3d06de396eae165cb161ccd60bf2e2ef9
alva <zwei22@wustl.edu>
2019-10-27 16:45:10 -0500

    ...

--- src/hw3/BPlusTree.java
+++ src/hw3/BPlusTree.java
@@ -4,15 +4,43 @@ package hw3;
 import hw1.Field;
 
 public class BPlusTree {
+	int pInner;
+	int pLeaf;
+	Node root;
+	
     
     public BPlusTree(int pInner, int pLeaf) {
     	//your code here
+    	this.pInner = pInner;
+    	this.pLeaf = pLeaf;
+    	this.root = null;
     }
     
     public LeafNode search(Field f) {
     	//your code here
-    	return null;
+    	//corner case
+    	if (root == null) {
+    		return null;
+    	}
+    	return searchHelper(f, this.root);
     }
+    private LeafNode searchHelper(Field f, Node root) {
+    	//base case
+    	if (root.isLeafNode() == true) {
+    		//iterate all entries in this leafNode to check whether f exist
+    		for (Entry entry: ((LeafNode)root).getEntries()) {
+    			if (entry.getField().equals(f)) {
+    				return (LeafNode)root;
+    			}
+    		}
+    		return null;
+    	}
+    	for (Field key : ((InnerNode)root).getKeys()) {
+    		if (k.compare(LTE, f))
+    	}
+    	
+    }
+    
     
     public void insert(Entry e) {
     	//your code here
